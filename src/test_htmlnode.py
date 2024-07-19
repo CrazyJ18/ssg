1c436bcc5442bfa3a9d6f40e68f26e5bc5de21c7
CrazyJ18 <jsteinke310@gmail.com>
2024-07-19 15:17:01 -0500

    add leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -22,6 +22,31 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("h1", "Title")
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+    def test_to_html2(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        target = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), target)
+
+    def test_to_html3(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        target = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), target)
+
+    def test_to_html4(self):
+        node = LeafNode(value="This is a raw paragraph of text.")
+        target = "This is a raw paragraph of text."
+        self.assertEqual(node.to_html(), target)
+
+    def test_to_html5(self):
+        node = LeafNode("img", props={"src":"url/of/image.jpg", "alt":"Description of image"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html6(self):
+        node = LeafNode("li", "Item 1")
+        target = "<li>Item 1</li>"
+        self.assertEqual(node.to_html(), target)
 
 
 if __name__ == "__main__":
