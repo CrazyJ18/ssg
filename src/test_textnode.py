4ad03fbd4442e93a1ec6c5a194f181ea221a315e
CrazyJ18 <jsteinke310@gmail.com>
2024-07-29 12:30:55 -0500

    add split nodes delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,48 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold", "www.boot.dev")
         node2 = TextNode("This is a text node", "bold")
         self.assertNotEqual(node, node2)
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        target = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" word", text_type_text)
+            ]
+        self.assertEqual(new_nodes, target)
+
+    def test_split_nodes_delimiter2(self):
+        node = TextNode("This is text with **bold words**", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        target = [
+            TextNode("This is text with ", text_type_text),
+            TextNode("bold words", text_type_bold),
+            TextNode("", text_type_text)
+            ]
+        self.assertEqual(new_nodes, target)
+
+    def test_split_nodes_delimiter3(self):
+        node = TextNode("This *word* is *italic*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        target = [
+            TextNode("This ", text_type_text),
+            TextNode("word", text_type_italic),
+            TextNode(" is ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode("", text_type_text)
+            ]
+        self.assertEqual(new_nodes, target)
+
+    def test_split_nodes_delimiter4(self):
+        node = TextNode("**Bold** move", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        target = [
+            TextNode("", text_type_text),
+            TextNode("Bold", text_type_bold),
+            TextNode(" move", text_type_text)
+            ]
+        self.assertEqual(new_nodes, target)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -12,3 +12,29 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        else:
+            split_text = node.text.split(delimiter)
+            split_text_len = len(split_text)
+            if split_text_len % 2 == 0:
+                raise Exception("no closing delimiter")
+            split_nodes = []
+            for i in range(split_text_len):
+                if i % 2 == 0:
+                    split_nodes.append(TextNode(split_text[i], text_type_text))
+                else:
+                    split_nodes.append(TextNode(split_text[i], text_type))
+            new_nodes.extend(split_nodes)
+    return new_nodes
