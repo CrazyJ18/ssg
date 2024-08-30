0b8c277b24ede5fc61eb8f7ce2754d6c0ada4567
CrazyJ18 <jsteinke310@gmail.com>
2024-08-30 11:18:13 -0500

    change how copy_dir deletes entries

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ import os, shutil
 
 
 def copy_dir(src, dst):
-    if os.path.exists(dst) == False:
+    if os.path.exists(dst):
+        for entry in os.listdir(dst):
+            entry_path = os.path.join(dst, entry)
+            if os.path.isfile(entry_path):
+                os.remove(entry_path)
+            else:
+                shutil.rmtree(entry_path)
+    else:
         os.mkdir(dst)
-    for entry in os.listdir(dst):
-        entry_path = os.path.join(dst, entry)
-        if os.path.isfile(entry_path):
-            os.remove(entry_path)
-        else:
-            shutil.rmtree(entry_path)
     for entry in os.listdir(src):
         entry_path = os.path.join(src, entry)
         if os.path.isfile(entry_path):
